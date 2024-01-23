34e457cfaca2b506d4b9580ac9e62e6ec3eb176b
Deligede <143736335+dl0520dl@users.noreply.github.com>
2024-01-23 20:44:19 +0800

    Add files via upload

--- String/BF.cpp
+++ String/BF.cpp
@@ -1,39 +1,33 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
 
-int BF(char *A, char *B){
-    int len1 = strlen(A);
-    int len2 = strlen(B);
+int BF(string A, string B){
+    int len1 = A.size();
+    int len2 = B.size();
     int i = 0, j = 0;
-    while (i < len1 && j < len2)
-    {
-        if (A[i] == B [j])
-        {
+    while (i < len1 && j < len2){
+        if (A[i] == B [j]){
             i++;
             j++;
         }
-        else
-        {
+        else{
             i = i - j + 1;
             j = 0;
         }
     }
-    if (j == len2)
-    {
+    if (j == len2){
         return i - j;
     }
-    else
-    {
+    else{
         return -1;
     }
 }
 
 int main()
 {
-    char *a = "abjsasadcab";
-    char *b = "cab";
+    string a = "abjsasadcab";
+    string b = "cab";
     int pos = BF(a, b);
-    printf("%d\n",pos);
-    system("pause");
+    cout << pos << endl;
     return 0;
 }
--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,81 +1,65 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
 
-
-void GetNext(char *B, int *next){
-    int len = strlen(B);
-    if (len == 1)
-    {
+void GetNext(string B, int *next){
+    int len = B.size();
+    if (len == 1){
         next[0] = -1;
         return;
     }
-    else
-    {
+    else{
         next[0] = -1;
         next[1] = 0;
         int j = 1;
         int k = 0;
-        while (j + 1 < len)
-        {
-            if(k == -1 || B[k] == B[j])
-            {
+        while (j + 1 < len){
+            if(k == -1 || B[k] == B[j]){
                 next[j + 1] = k + 1; 
                 j++;
                 k++;
             }
-            else
-            {
+            else{
                 k = next[k];
             }
         }  
     }
-    
 }
 
-int kmp(char *A, char *B){
-    if (A == NULL || B == NULL)
-    {
+int kmp(string A, string B){
+    if (A.size() == 0 || B.size() == 0){
         return -1;
     }
-    else
-    {
+    else{
         int i = 0;
         int j = 0;
-        int len1 = strlen(A);
-        int len2 = strlen(B);
+        int len1 = A.size();
+        int len2 = B.size();
         int next[len2];
         GetNext(B, next);
         while (i < len1 && j < len2)
         {
-            if (A[i] == B[j] || j == -1)
-            {
+            if (A[i] == B[j] || j == -1){
                 i++;
                 j++;
             }
-            else
-            {
+            else{
                 j = next [j];
             }
         }
-         if (j == len2)
-         {
+         if (j == len2){
             return i - j;
          }
-         else
-         {
+         else{
             return -1;
          }
     }
 }
-
-
 
 int main()
 {
     char *a = "abjsasadcab";
     char *b = "cab";
     int pos = kmp(a, b);
-    printf("%d\n",pos);
-    system("pause");
+    cout << pos << endl;
     return 0;
 }
